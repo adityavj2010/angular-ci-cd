170b8af4a1f9182423a172b03eb6001ce5a42aed
adityavj2010 <adityavj2010@gmail.com>
2020-03-29 23:51:55 +0530

    Modified travis.yml

--- karma.conf.js
+++ karma.conf.js
@@ -2,6 +2,7 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
+  
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
@@ -19,6 +20,12 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/angular-ci-cd'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
+    },
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
